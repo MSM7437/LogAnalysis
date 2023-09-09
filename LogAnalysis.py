8b983a1101421bf0bc3a135bdab3d6ec9204580e
ryanbeck144 <marcusbeck28@gmail.com>
2023-09-09 15:49:57 -0500

    created 6 months of log code

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -1,23 +1,41 @@
-import urllib.request  # Import the urllib.request module for making HTTP requests.
+import os
+import re
+from datetime import datetime, timedelta, timezone
+import urllib.request
 
-# Replace 'your_url_here' with the actual URL of the log file you want to download
-url = 'https://s3.amazonaws.com/tcmg476/http_access_log'  # Set the URL of the log file to download.
+# URL of the log file
+url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 
-try:
-    # Open the URL and read its content
-    with urllib.request.urlopen(url) as response:
-        # Check if the HTTP response status code is 200 (OK)
-        if response.status == 200:
-            # Open a local file for writing the log data in binary mode ('wb')
-            with open('downloaded_log_file.log', 'wb') as file:
-                # Read the content from the response and write it to the local file
-                file.write(response.read())
-            print("Log file downloaded successfully.")
-        else:
-            # If the status code is not 200, print an error message
-            print(f"Failed to download log file. Status code: {response.status}")
+# Local cache file path
+local_cache_file = "http_access_log.txt"
 
-except Exception as e:
-    # Handle any exceptions that may occur during the process
-    print(f"An error occurred: {str(e)}")
+def is_cache_outdated(cache_file):
+    if not os.path.exists(cache_file):
+        return True
+    last_modified_time = datetime.fromtimestamp(os.path.getmtime(cache_file))
+    current_time = datetime.now()
+    return (current_time - last_modified_time) > timedelta(hours=24)
+
+if not os.path.exists(local_cache_file) or is_cache_outdated(local_cache_file):
+    urllib.request.urlretrieve(url, local_cache_file)
+
+log_entry_pattern = r'\[(\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2} -\d{4})\]'
+
+utc_timezone = timezone.utc
+
+start_date = datetime(1995, 10, 11, tzinfo=utc_timezone)
+
+six_months_ago = start_date - timedelta(days=180)
+
+total_requests = 0
+
+with open(local_cache_file, 'r') as log_file:
+    for line in log_file:
+        match = re.search(log_entry_pattern, line)
+        if match:
+            log_date = datetime.strptime(match.group(1), '%d/%b/%Y:%H:%M:%S %z')
+            if log_date >= six_months_ago:
+                total_requests += 1
+
+print(f"Total requests in the past 6 months: {total_requests}")
 
